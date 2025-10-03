def1f7da90cbad6d92bcc09bcc0484a07a2313a0
LorenzoLopesSilva <lorenzo.lopes0808@gmail.com>
2025-10-03 13:23:18 -0300

    update

---
+++ desafio/teste.py
@@ -0,0 +1,43 @@
+edvs = []
+
+contatos = [
+    {
+        "nome": "Lorenzo",
+        "telefone": "19983659999",
+        "edv": "1234",
+        "email": "lorenzo@teste"
+    },
+    {
+        "nome": "Wilson",
+        "telefone": "1998070707",
+        "edv": "9999",
+        "email": "wilson@email"
+    }
+]
+
+for i in contatos:
+    edvs.append(i['edv'])
+
+edv = input("edv: ")
+
+index = edvs.index(edv)
+
+print(contatos[index])
+
+
+import reportlab.lib.pagesizes as pagesizes
+
+from reportlab.pdfgen import canvas
+
+page_size = pagesizes.letter
+
+canvas = canvas.Canvas('python_b\desafio','example.pdf')
+
+canvas.setPageSize(page_size)
+
+text = str(contatos)
+
+canvas.drawString(100, 750, text)
+
+canvas.showPage()
+canvas.save()
